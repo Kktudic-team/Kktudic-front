9332dbfb64e42f6b412d35a0ea211d8d879dfff3
박은혁 <111491749+JarikiSungnyung@users.noreply.github.com>
2023-04-15 20:01:10 +0900

    Update NoticeView.vue
    Add Keep-alive

--- src/views/NoticeView.vue
+++ src/views/NoticeView.vue
@@ -1,22 +1,24 @@
 <template>
   <div class="cont">
     <div class="box">
-      <table class="tb">
-        <thead>
-          <tr class="thr">
-            <th class="num">번호</th>
-            <th class="name">이름</th>
-            <th class="day">작성 날짜</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="notice in notices" :key="notice.id" @click="goToDetail(notice.id)">
-            <td class="num">{{ notice.id }}</td>
-            <td class="name">{{ notice.title }}</td>
-            <td class="day">{{ notice.createdAt }}</td>
-          </tr>
-        </tbody>
-      </table>
+      <keep-alive>
+        <table class="tb">
+          <thead>
+            <tr class="thr">
+              <th class="num">번호</th>
+              <th class="name">이름</th>
+              <th class="day">작성 날짜</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="notice in notices" :key="notice.id" @click="goToDetail(notice.id)">
+              <td class="num">{{ notice.id }}</td>
+              <td class="name">{{ notice.title }}</td>
+              <td class="day">{{ notice.createdAt }}</td>
+            </tr>
+          </tbody>
+        </table>
+      </keep-alive>
     </div>
   </div>
 </template>
